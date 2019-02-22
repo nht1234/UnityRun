aef9d43ff95e1dfd89f90eb0eeda89e064f49f41
nht1234 <47805750+nht1234@users.noreply.github.com>
2019-02-22 13:15:41 +0900

    New

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float Speed = 5f;//캐릭터 이동속도
+    public float JumpPower = 100;
+    bool isJump = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,30 @@ public class Player : MonoBehaviour
                                                            //인자: Vector3(x, y, z)
                                                            //Speed * Time.deltaTime 60프레임일때, 1프레임 이동거리
                                                            //= 초속 5m * (1/60)
-        
+        if (transform.position.y < 0.001f)
+        {
+            isJump = false;
+
+            GetComponent<Animator>().SetBool("Jump", false);
+        }
+        if (Input.GetMouseButtonDown(0) && !isJump)
+        {
+            GetComponent<Rigidbody>().AddForce(0, JumpPower, 0);
+            isJump = true;
+
+            GetComponent<Animator>().SetBool("Jump", true);
+        }
+
+    }
+
+    // 오브젝트가 충돌하면, 유니티에서 자동으로 호출되는 함수
+    private void OnCollisionEnter(Collision collision)
+    {
+        //만약, 충돌한 오브젝트의 태그가 Block 이면
+        if (collision.transform.tag == "Block")
+        {
+            // 게임 오버를 처리한다.
+            SceneManager.LoadScene(0);
+        }
     }
 }
